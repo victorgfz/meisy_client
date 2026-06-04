8009ddaa888bb83ae4a21b29c2cf10c527685ad6
Victor Gomes Ferraz <victor.sjb.2002@gmail.com>
2026-06-04 16:41:06 -0300

    fix: reports

--- src/features/dashboard/components/previous-months-report-card.tsx
+++ src/features/dashboard/components/previous-months-report-card.tsx
@@ -1,5 +1,7 @@
 import { TrendingDown, TrendingUp } from "lucide-react"
 import type { PreviousMonthReport } from "../types/reports.types"
+import { subMonths, format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 interface PreviousMonthsReportCardProps {
     data: PreviousMonthReport[]
@@ -9,6 +11,7 @@ function PreviousMonthsReportCard({data}: PreviousMonthsReportCardProps) {
     const formattedCurrency = (value: number) => new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value)
     const isIncrease = (value: number) => value > 1 ? true : false
     const formattedVariationRate = (value: number) => (value -1) * 100
+    const monthName = (value: number) => format(subMonths(new Date(), value) , 'MMMM', {locale: ptBR}) 
 
     return (<div className="bg-white border border-gray-200 rounded-xl shadow-sm p-5 transition-shadow flex flex-col gap-6">
         <div className="py-2">
@@ -16,9 +19,9 @@ function PreviousMonthsReportCard({data}: PreviousMonthsReportCardProps) {
             <div className="flex flex-col gap-2 justify-start items-center">
 
             {data && data.length > 0 ?
-            data.map(item => (<div className="flex items-center justify-between w-full bg-gray-100 rounded p-4">
+            data.map((item, index) => (<div className="flex items-center justify-between w-full bg-gray-100 rounded p-4">
                 <div>
-                    <h3 className="text-md text-gray-800 font-bold">Nome do mes</h3>
+                    <h3 className="text-md text-gray-800 font-bold capitalize">{monthName(index+1)}</h3>
                     <p className="text-xs text-gray-400 font-light">{item.quantityOfOrders} pedidos ({item.quantityOfCompletedOrders} concluídos)</p>
                 </div>
                 <div className="flex justify-end items-start divide-x-[1px] divide-gray-300">
@@ -57,7 +60,7 @@ function PreviousMonthsReportCard({data}: PreviousMonthsReportCardProps) {
                         
                     </div>
                     <div className="text-center flex flex-col items-center gap-1 px-2">
-                        <p className="text-[8px] uppercase font-bold">Custos</p>
+                        <p className="text-[8px] uppercase font-bold">Lucro</p>
                         <p className="text-xs">{formattedCurrency(item.totalProfit)}</p>
                         
                             {isIncrease(item.totalProfitVariationRate) ?
